4bdd551e45f24a756f80a434a55cfda8f5e232bc
vivek1242 <bhosvivek123@gmail.com>
2023-07-02 16:40:32 +0530

    Modified About.js and Policy.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,11 +3,42 @@ import Layout from "../components/Layout/Layout";
 
 const About = () => {
   return (
-    <>
-      <Layout>
-        <h1>About</h1>
-      </Layout>
-    </>
+    <Layout>
+      <div className="container">
+        <h1 className="text-center">Welcome to Vtech Ecomm</h1>
+        <p>
+          At Vtech Ecomm, we strive to provide the best online shopping
+          experience for our customers. We offer a wide range of high-quality
+          products from various categories, including electronics, fashion, home
+          decor, and more.
+        </p>
+        <p>
+          Our mission is to make shopping convenient, enjoyable, and secure.
+          With our user-friendly website and seamless checkout process, you can
+          easily browse through our extensive product catalog, find exactly what
+          you need, and make a purchase with confidence.
+        </p>
+        <p>
+          We are committed to delivering excellent customer service. Our
+          dedicated support team is available to assist you with any inquiries,
+          product recommendations, or order assistance. We value your
+          satisfaction and aim to exceed your expectations at every step of your
+          shopping journey.
+        </p>
+        <p>
+          Shopping at Vtech Ecomm is not just about buying products; it's about
+          discovering new trends, exploring exciting deals, and connecting with
+          a community of like-minded shoppers. Stay updated with our latest
+          arrivals, exclusive offers, and promotions by subscribing to our
+          newsletter and following us on social media.
+        </p>
+        <p>
+          Thank you for choosing Vtech Ecomm. We look forward to serving you and
+          providing you with an exceptional online shopping experience. Happy
+          shopping!
+        </p>
+      </div>
+    </Layout>
   );
 };
 
--- src/pages/Policy.js
+++ src/pages/Policy.js
@@ -3,11 +3,57 @@ import Layout from "../components/Layout/Layout";
 
 const Policy = () => {
   return (
-    <>
-      <Layout>
-        <div>Policy</div>
-      </Layout>
-    </>
+    <Layout>
+      <div className="container my-3 policy-container">
+        <h1 className="text-center">Policy</h1>
+        <h2>Privacy Policy</h2>
+        <p>
+          At Vtech Ecomm, we value your privacy and are committed to protecting
+          your personal information. Our Privacy Policy outlines how we collect,
+          use, and safeguard your data when you use our website or make a
+          purchase. We ensure that your information is securely stored and only
+          used for the purposes specified in our policy. Please read our Privacy
+          Policy for more details.
+        </p>
+
+        <h2>Shipping Policy</h2>
+        <p>
+          We strive to provide fast and reliable shipping for all orders placed
+          on our website. Our Shipping Policy outlines the details regarding
+          shipping methods, delivery times, and any applicable fees or
+          restrictions. We work with trusted shipping partners to ensure your
+          orders are delivered safely and on time. For more information, please
+          refer to our Shipping Policy.
+        </p>
+
+        <h2>Return Policy</h2>
+        <p>
+          Your satisfaction is our priority, and we understand that sometimes
+          returns may be necessary. Our Return Policy provides information on
+          how to initiate a return, eligibility criteria, and any applicable
+          fees or conditions. We aim to make the return process as smooth as
+          possible for our customers. Please review our Return Policy for more
+          details.
+        </p>
+
+        <h2>Terms and Conditions</h2>
+        <p>
+          By using our website and making a purchase, you agree to abide by our
+          Terms and Conditions. Our Terms and Conditions outline the rules and
+          guidelines for using our website, making payments, and other important
+          aspects of the customer-seller relationship. It is important to read
+          and understand our Terms and Conditions before engaging in any
+          transactions on our website.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          If you have any questions or concerns regarding our policies, please
+          don't hesitate to contact our customer support team. We are here to
+          assist you and provide any clarification you may need.
+        </p>
+      </div>
+    </Layout>
   );
 };
 
